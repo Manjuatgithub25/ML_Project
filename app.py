6db2e2dbe43f467041bcbd86fa5da5555284ac6b
Manjuatgithub25 <manjunathyashram5@gmail.com>
2025-04-08 20:11:27 +0530

    App.py correction

--- app.py
+++ app.py
@@ -1,18 +1,15 @@
 import streamlit as st
 import pandas as pd
-import pickle  # Assuming you're using a trained model saved as a .pkl file
+import pickle 
 
 with open("artifact/preprocessor.pkl", "rb") as f:
     preprocessor = pickle.load(f)
 
-# Load the trained model
 with open("artifact/model.pkl", "rb") as f:
     model = pickle.load(f)
 
-# Title
 st.title("Student Performance Prediction")
 
-# User Inputs
 st.sidebar.header("Input Features")
 
 # Define input fields
@@ -26,17 +23,9 @@ def user_inputs():
     reading_score = st.sidebar.slider("reading_score", 0, 100, 50)
     writing_score = st.sidebar.slider("writing_score", 0, 100, 50)
 
-
-    # Convert categorical data if required
-    # gender = 1 if gender == "male" else 0
-
-    # return pd.DataFrame([[gender, study_time, absences, past_scores]], 
-    #                     columns=["Gender", "Study_Time", "Absences", "Past_Scores"])
-
     return pd.DataFrame([[gender, ethnicity, parent_education, lunch_type, course_taken, reading_score, writing_score]], 
                         columns=["gender", "race_ethnicity", "parental_level_of_education", "lunch", "test_preparation_course", "reading_score", "writing_score"])
 
-# Get user inputs
 input_df = user_inputs()
 
 # Predict button
